b953b824cf398f3fb4c428e36a4e055417eea10d
swami <swami.gms@gmail.com>
2021-02-07 13:31:00 +0530

    updating microservice call with https

--- movie-catalog-service/src/main/java/com/swamicodes/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/swamicodes/moviecatalogservice/resources/MovieCatalogResource.java
@@ -26,9 +26,9 @@ public class MovieCatalogResource {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
-		UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+		UserRating userRating = restTemplate.getForObject("https://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
 		return userRating.getRatingsList().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("https://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getMovieName(), "Movie Description", rating.getRating());
 		}).collect(Collectors.toList());
 	}
